f69be32468a3f91b30d2c5e2979db09db116babe
Ruixiang Du <rdu@rdu-ThinkPad.(none)>
2014-03-13 16:04:39 -0400

    add maps

--- fallrisk_tools/checklist_status/src/checklist_status_server.cpp
+++ fallrisk_tools/checklist_status/src/checklist_status_server.cpp
@@ -8,7 +8,7 @@ bool getChecklistStatus(checklist_status::ChecklistStatusSrv::Request  &req,
 //  ROS_INFO("request: x=%ld, y=%ld", (long int)req.a, (long int)req.b);
 //  ROS_INFO("sending back response: [%ld]", (long int)res.sum);
 //  return true;
-    res.luminosity=20;
+    res.luminosity=300;
     ROS_INFO("Checklist update responded!");
     return true;
 }
