bd254194d15814dbda146ba61d4fe7056193970b
rxdu <ruixiang.du@gmail.com>
2014-04-23 16:18:26 -0400

    changed the callback function declaration

--- fallrisk_mapping_navigation/simple_nav_goal/src/rviz_gui_navigation_goal.cpp
+++ fallrisk_mapping_navigation/simple_nav_goal/src/rviz_gui_navigation_goal.cpp
@@ -95,9 +95,9 @@ int main(int argc, char** argv){
 #include <tf/transform_datatypes.h>
 #include <math.h>
 
-  class rviz_gui_handler
-  {
-    private:
+class rviz_gui_handler
+{
+private:
 
     geometry_msgs::Twist cmd;
     geometry_msgs::PoseStamped goal; // Goal position message of geometry_msgs::PoseStamped type
@@ -116,99 +116,99 @@ int main(int argc, char** argv){
     double yawCurrent;
     geometry_msgs::Quaternion quatMsg;
 
-    public:
+public:
 
-   ~rviz_gui_handler();
+    ~rviz_gui_handler();
     void init();
-    void subCB(const geometry_msgs::Twist& msg);
+    void subCB(geometry_msgs::TwistConstPtr msg);
     void getCurrentPosition();
     void setGoalPosition();
     void publishGoal();
-  };
+};
 
-  void rviz_gui_handler::init()
-      {
-        direction_pub = nh.advertise<geometry_msgs::Twist>("/cmd_vel_mux/input/teleop", 8);
-        goal_pub = nh.advertise<geometry_msgs::PoseStamped> ("/move_base_simple/goal",8);
-        gui_sub = nh.subscribe("/some_topic", 8, &rviz_gui_handler::subCB,this);
+void rviz_gui_handler::init()
+{
+    direction_pub = nh.advertise<geometry_msgs::Twist>("/cmd_vel_mux/input/teleop", 8);
+    goal_pub = nh.advertise<geometry_msgs::PoseStamped> ("/move_base_simple/goal",8);
+    gui_sub = nh.subscribe("/some_topic", 8, &rviz_gui_handler::subCB,this);
 
-        dist = 0.0;
-        angular = 0.0;
-        xCurrent = 0.0;
-        yCurrent = 0.0;
-        zCurrent = 0.0;
-        yawCurrent = 0.0;
+    dist = 0.0;
+    angular = 0.0;
+    xCurrent = 0.0;
+    yCurrent = 0.0;
+    zCurrent = 0.0;
+    yawCurrent = 0.0;
 
-      }
+}
 
-      rviz_gui_handler::~rviz_gui_handler()
-      {
+rviz_gui_handler::~rviz_gui_handler()
+{
 
-      }
+}
 
-  void rviz_gui_handler::subCB(const geometry_msgs::Twist& msg)
-  {
-      ROS_INFO("Inside callback Function");
-	  double linear = msg.linear.x;
-	  double time = 0.5;
+void rviz_gui_handler::subCB(geometry_msgs::TwistConstPtr msg)
+{
+    ROS_INFO("Inside callback Function");
+    double linear = msg.linear.x;
+    double time = 0.5;
 
-	  dist = linear * time;
-	  angular = msg.angular.z;
-  }
+    dist = linear * time;
+    angular = msg.angular.z;
+}
 
-  void rviz_gui_handler::getCurrentPosition()
-  {
-      tf::StampedTransform transform;
-      try{
-          listener.waitForTransform("/base_link", "/map",ros::Time(0), ros::Duration(10.0));
-          listener.lookupTransform("/base_link", "/map",ros::Time(0), transform);
-      }
-      catch (tf::TransformException ex){
-          ROS_ERROR("%s",ex.what());
-      }
-      xCurrent = transform.getOrigin().x();
-      yCurrent = transform.getOrigin().y();
-      zCurrent = transform.getOrigin().z();
-      tf::Quaternion quat = transform.getRotation();
-      tf::quaternionTFToMsg(quat, quatMsg);
-      yawCurrent = tf::getYaw(quatMsg);
+void rviz_gui_handler::getCurrentPosition()
+{
+    tf::StampedTransform transform;
+    try{
+        listener.waitForTransform("/base_link", "/map",ros::Time(0), ros::Duration(10.0));
+        listener.lookupTransform("/base_link", "/map",ros::Time(0), transform);
+    }
+    catch (tf::TransformException ex){
+        ROS_ERROR("%s",ex.what());
+    }
+    xCurrent = transform.getOrigin().x();
+    yCurrent = transform.getOrigin().y();
+    zCurrent = transform.getOrigin().z();
+    tf::Quaternion quat = transform.getRotation();
+    tf::quaternionTFToMsg(quat, quatMsg);
+    yawCurrent = tf::getYaw(quatMsg);
 
-       ROS_INFO("%f,%f",xCurrent,yCurrent);
-  }
+    ROS_INFO("%f,%f",xCurrent,yCurrent);
+}
 
-  void rviz_gui_handler::setGoalPosition()
-  {
+void rviz_gui_handler::setGoalPosition()
+{
 
-      (goal.header).frame_id = "map";
-	  (goal.header).stamp = ros::Time::now();
-	  (goal.pose).position.x = xCurrent + (dist) * cos(yawCurrent);
-	  (goal.pose).position.y = yCurrent + (dist) * sin(yawCurrent);
-	  (goal.pose).position.z = zCurrent;
-	  (goal.pose).orientation = quatMsg;
+    (goal.header).frame_id = "map";
+    (goal.header).stamp = ros::Time::now();
+    (goal.pose).position.x = xCurrent + (dist) * cos(yawCurrent);
+    (goal.pose).position.y = yCurrent + (dist) * sin(yawCurrent);
+    (goal.pose).position.z = zCurrent;
+    (goal.pose).orientation = quatMsg;
 
-	  (cmd.linear).x = 0;
-	  (cmd.linear).y = 0;
-	  (cmd.linear).z = 0;
+    (cmd.linear).x = 0;
+    (cmd.linear).y = 0;
+    (cmd.linear).z = 0;
 
-	  (cmd.angular).x = 0;
-	  (cmd.angular).y = 0;
-	  (cmd.angular).z = angular;
-  }
+    (cmd.angular).x = 0;
+    (cmd.angular).y = 0;
+    (cmd.angular).z = angular;
+}
 
-  void rviz_gui_handler::publishGoal()
-  {
-	  direction_pub.publish(cmd);
-	  goal_pub.publish(goal);
-  }
+void rviz_gui_handler::publishGoal()
+{
+    direction_pub.publish(cmd);
+    goal_pub.publish(goal);
+}
 
-  int main(int argc, char** argv)
-  {
+int main(int argc, char** argv)
+{
     ros::init(argc, argv, "rviz_gui_handler");
 
     rviz_gui_handler handler;
     handler.init();
 
-     ros::Rate rate(10.0);
+    ros::Rate rate(10.0);
 
     while(ros::ok())
     {
@@ -219,4 +219,4 @@ int main(int argc, char** argv){
     }
 
     return(0);
-  }
+}
